e438619614abe55a5283df1614687b87cd95c421
kosteg <kostyrkoevgeniy@yandex.ru>
2023-05-08 20:38:24 +0300

    axios, nookies, form login (ant)

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,6 +1,36 @@
 import React from "react";
-import styles from "/LoginFrom.module.scss";
+import styles from "./Auth.module.scss";
+import { Button, Form, Input } from "antd";
 
 export const LoginForm: React.FC = () => {
-  return <div className={styles.root}>Форма авторизации</div>;
+  const onSubmit = (values: any) => {
+    console.log(values);
+  };
+  return (
+    <div className={styles.formBlock}>
+      <Form name="basic" labelCol={{ span: 8 }} onFinish={onSubmit}>
+        <Form.Item
+          label="E-Mail"
+          name="email"
+          rules={[{ required: true, message: "Укажите почту!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Укажите пароль!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Войти
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
