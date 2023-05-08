da2a1f3c1c00dd17d41b2f897f83ea3ecabf1d0e
kosteg <kostyrkoevgeniy@yandex.ru>
2023-05-08 19:58:07 +0300

    init, add login page and style global, refa

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "/LoginFrom.module.scss";
+
+export const LoginForm: React.FC = () => {
+  return <div className={styles.root}>Форма авторизации</div>;
+};
